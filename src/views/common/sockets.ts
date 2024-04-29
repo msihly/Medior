ffd26229a108110ed5520090e215f6b30cd065a0
msihly <43391570+msihly@users.noreply.github.com>
2024-04-29 18:23:05 -0400

    • Refactored useSockets to use streamlined makeSocket function

--- src/views/common/sockets.ts
+++ src/views/common/sockets.ts
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { SocketEmitEvent, SocketEmitEvents } from "server";
 import { useStores } from "store";
 import { setupSocketIO, socket } from "utils";
 
@@ -12,26 +13,25 @@ export const useSockets = ({ view }: UseSocketsProps) => {
   const { carouselStore, fileCollectionStore, fileStore, homeStore, importStore, tagStore } =
     useStores();
 
+  const debugLog = (
+    eventName: SocketEmitEvent,
+    eventArgs: Parameters<SocketEmitEvents[SocketEmitEvent]>[0]
+  ) => debug && console.debug(`[Socket] ${eventName}`, eventArgs);
+
+  const makeSocket = <T extends SocketEmitEvent>(
+    eventName: T,
+    callback: (args: Parameters<SocketEmitEvents[T]>[0]) => void
+  ) =>
+    // @ts-expect-error
+    socket.on(eventName, (eventArgs) => {
+      debugLog(eventName, eventArgs);
+      callback(eventArgs);
+    });
+
   const setupSockets = () => {
     setupSocketIO();
 
-    socket.on("collectionCreated", ({ collection }) => {
-      if (debug) console.debug("[Socket] collectionCreated", { collection });
-      if (view !== "carousel") fileCollectionStore._addCollection(collection);
-    });
-
-    socket.on("collectionDeleted", ({ collectionId }) => {
-      if (debug) console.debug("[Socket] collectionDeleted", { collectionId });
-      if (view !== "carousel") fileCollectionStore._deleteCollection(collectionId);
-    });
-
-    socket.on("collectionUpdated", ({ collectionId, updates }) => {
-      if (debug) console.debug("[Socket] collectionUpdated", { collectionId, updates });
-      if (view !== "carousel") fileCollectionStore.getById(collectionId)?.update(updates);
-    });
-
-    socket.on("filesDeleted", ({ fileHashes, fileIds }) => {
-      if (debug) console.debug("[Socket] filesDeleted", { fileIds });
+    makeSocket("filesDeleted", ({ fileHashes, fileIds }) => {
       if (view === "carousel") carouselStore.removeFiles(fileIds);
       else {
         if (view === "home") importStore.addDeletedFileHashes(fileHashes);
@@ -40,33 +40,22 @@ export const useSockets = ({ view }: UseSocketsProps) => {
       }
     });
 
-    socket.on("filesUpdated", ({ fileIds, updates }) => {
-      if (debug) console.debug("[Socket] filesUpdated", { fileIds, updates });
+    makeSocket("filesUpdated", ({ fileIds, updates }) => {
       fileStore.updateFiles(fileIds, updates);
 
-      const updatedKeys = Object.keys(updates);
-      if (
-        view !== "carousel" &&
-        (updatedKeys.some((k) => ["isArchived", "tagIds"].includes(k)) ||
-          updatedKeys.includes(homeStore.sortValue.key))
-      ) {
-        homeStore.loadFilteredFiles();
+      if (view !== "carousel") {
+        const updatedKeys = Object.keys(updates);
+        const shouldReload =
+          updatedKeys.some((k) => ["isArchived", "tagIds"].includes(k)) ||
+          updatedKeys.includes(homeStore.sortValue.key);
+        if (shouldReload) homeStore.loadFilteredFiles();
       }
     });
 
-    socket.on("fileTagsUpdated", ({ addedTagIds, batchId, fileIds, removedTagIds }) => {
-      if (debug)
-        console.debug("[Socket] fileTagsUpdated", {
-          addedTagIds,
-          batchId,
-          fileIds,
-          removedTagIds,
-        });
-
+    makeSocket("fileTagsUpdated", ({ addedTagIds, batchId, fileIds, removedTagIds }) => {
       fileStore.updateFileTags({ addedTagIds, fileIds, removedTagIds });
 
       if (view !== "carousel") homeStore.loadFilteredFiles();
-
       if (view === "home" && batchId?.length > 0)
         importStore.editBatchTags({
           addedIds: addedTagIds,
@@ -75,67 +64,59 @@ export const useSockets = ({ view }: UseSocketsProps) => {
         });
     });
 
-    socket.on("reloadFiles", () => {
-      if (debug) console.debug("[Socket] reloadFiles");
+    makeSocket("reloadFiles", () => {
       if (view === "carousel") fileStore.loadFiles({ fileIds: carouselStore.selectedFileIds });
       else homeStore.loadFilteredFiles();
     });
 
-    socket.on("reloadTags", () => {
-      if (debug) console.debug("[Socket] reloadTags");
-      tagStore.loadTags();
-    });
+    makeSocket("reloadTags", () => tagStore.loadTags());
 
-    socket.on("tagCreated", ({ tag }) => {
-      if (debug) console.debug("[Socket] tagCreated", { tag });
-      tagStore._addTag(tag);
-    });
+    makeSocket("tagCreated", ({ tag }) => tagStore._addTag(tag));
 
-    socket.on("tagDeleted", ({ tagId }) => {
-      if (debug) console.debug("[Socket] tagDeleted", { tagId });
+    makeSocket("tagDeleted", ({ tagId }) => {
       tagStore._deleteTag(tagId);
       if (view === "home") importStore.editBatchTags({ removedIds: [tagId] });
-      if (view !== "carousel") {
-        homeStore.removeDeletedTag(tagId);
-        homeStore.loadFilteredFiles();
-      }
+      if (view !== "carousel") homeStore.loadFilteredFiles();
     });
 
-    socket.on("tagsUpdated", (tags) => {
-      if (debug) console.debug("[Socket] tagsUpdated", { tags });
+    makeSocket("tagMerged", ({ oldTagId }) => {
+      if (view === "home") importStore.editBatchTags({ removedIds: [oldTagId] });
+    });
+
+    makeSocket("tagsUpdated", (tags) => {
       tags.forEach((t) => tagStore.getById(t.tagId)?.update(t.updates));
       if (view !== "carousel") homeStore.loadFilteredFiles();
     });
 
-    if (view === "carousel")
-      socket.on("filesArchived", ({ fileIds }) => {
-        if (debug) console.debug("[Socket] filesArchived", { fileIds });
-        carouselStore.removeFiles(fileIds);
-      });
+    if (view === "carousel") {
+      makeSocket("filesArchived", ({ fileIds }) => carouselStore.removeFiles(fileIds));
+    } else {
+      makeSocket("collectionCreated", ({ collection }) =>
+        fileCollectionStore._addCollection(collection)
+      );
 
-    if (view !== "carousel") {
-      socket.on("importBatchCompleted", () => {
-        if (debug) console.debug("[Socket] importBatchCompleted");
+      makeSocket("collectionDeleted", ({ collectionId }) =>
+        fileCollectionStore._deleteCollection(collectionId)
+      );
+
+      makeSocket("collectionUpdated", ({ collectionId, updates }) =>
+        fileCollectionStore.getById(collectionId)?.update(updates)
+      );
+
+      makeSocket("importBatchCompleted", () => {
         if (!importStore.isImportManagerOpen) homeStore.loadFilteredFiles();
       });
 
-      socket.on("reloadFileCollections", () => {
-        if (debug) console.debug("[Socket] reloadFileCollections");
+      makeSocket("reloadFileCollections", () => {
         if (fileCollectionStore.isManagerOpen) fileCollectionStore.listFilteredCollections();
       });
     }
 
-    if (view === "home")
-      socket.on("reloadImportBatches", () => {
-        if (debug) console.debug("[Socket] reloadImportBatches");
-        importStore.loadImportBatches();
-      });
+    if (view === "home") makeSocket("reloadImportBatches", () => importStore.loadImportBatches());
   };
 
   useEffect(() => {
     setupSockets();
-    return () => {
-      socket.disconnect();
-    };
+    return () => (socket.disconnect(), null);
   }, []);
 };
